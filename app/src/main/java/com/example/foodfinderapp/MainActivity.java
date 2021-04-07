22663830de4a512d9cfc5475bccfd07b3b8056a5
Matthew Held <mjheld@clemson.edu>
2021-04-07 13:05:15 -0400

    Got navigation working

--- app/src/main/java/com/example/foodfinderapp/MainActivity.java
+++ app/src/main/java/com/example/foodfinderapp/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.foodfinderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hello Andrew!
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(SavedBusinessesFragment.newInstance("", ""));
+
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_saved_businesses:
+                            openFragment(SavedBusinessesFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_nearby_businesses:
+                            openFragment(NearbyBusinessesFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
 }
