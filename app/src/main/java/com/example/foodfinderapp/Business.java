7f45e77c47aaec81bfc739163e56d3f8eca7410c
Matthew Held <mjheld@clemson.edu>
2021-04-09 17:41:53 -0400

    Added backend for saving businesses

--- app/src/main/java/com/example/foodfinderapp/Business.java
+++ app/src/main/java/com/example/foodfinderapp/Business.java
@@ -8,18 +8,31 @@ import java.util.ArrayList;
  */
 public class Business implements Serializable {
 
+    private static int businessNum = 0;
+
+    private int m_index;
     private String m_name;
     private String m_description;
     private float m_lat;
     private float m_lng;
     private ArrayList<Reward> rewards = new ArrayList<>();
+    private boolean m_isSaved = false;
 
     // Constructor
     public Business (String name, String description, float lat, float lng) {
+        // Index updated statically
+        m_index = businessNum;
+        businessNum++;
+
         m_name = name;
         m_description = description;
         m_lat = lat;
         m_lng = lng;
+    }
+
+    // Getter for index of business
+    public int getIndex () {
+        return m_index;
     }
 
     // Getter for name of business
@@ -40,6 +53,16 @@ public class Business implements Serializable {
     // Getter for longitude of business
     public float getLng () {
         return m_lng;
+    }
+
+    // Getter to see if business is saved
+    public boolean getIfSaved () {
+        return m_isSaved;
+    }
+
+    // Setter for saving business
+    public void setIfSaved (boolean isSaved) {
+        m_isSaved = isSaved;
     }
 
     // Adds a reward to a business
--- app/src/main/java/com/example/foodfinderapp/BusinessActivity.java
+++ app/src/main/java/com/example/foodfinderapp/BusinessActivity.java
@@ -2,15 +2,21 @@ package com.example.foodfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  *   The activity for a business page
  */
 public class BusinessActivity extends AppCompatActivity {
 
+    Database database;
+    Business business;
+
     TextView businessName;
     TextView businessDescription;
+    Button setIfSavedBtn;
 
     // Called when activity is created
     @Override
@@ -20,14 +26,42 @@ public class BusinessActivity extends AppCompatActivity {
 
         businessName = (TextView) findViewById(R.id.businessNameTV);
         businessDescription = (TextView) findViewById(R.id.businessDescriptionTV);
+        setIfSavedBtn = (Button) findViewById(R.id.setIfSavedBtn);
 
         // Check if business object was successfully passed
         if (getIntent().getExtras() != null) {
-            Business business = (Business) getIntent().getSerializableExtra("business");
+            database = Database.getInstance();
+            business = (Business) getIntent().getSerializableExtra("business");
 
             // Set values on page
             businessName.setText(business.getName());
             businessDescription.setText(business.getDescription());
+            updateSetIfSavedBtn();
+        }
+    }
+
+    // Called when setIfSavedBtn is clicked
+    public void setIfSavedBtnClicked (android.view.View view) {
+        // Update database
+        database.setIfBusinessSaved(business, !database.getBusiness(business.getIndex()).getIfSaved());
+
+        // Update setIfSavedBtn
+        updateSetIfSavedBtn();
+
+        // Show toast indicating change
+        if (database.getBusiness(business.getIndex()).getIfSaved() == false) {
+            Toast.makeText(this, "Business Removed from Saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Business Added to Saved", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Changes the look of the setIfSavedBtn depending if the business is saved or not
+    private void updateSetIfSavedBtn () {
+        if (database.getBusiness(business.getIndex()).getIfSaved() == false) {
+            setIfSavedBtn.setText("+");
+        } else {
+            setIfSavedBtn.setText("x");
         }
     }
 
