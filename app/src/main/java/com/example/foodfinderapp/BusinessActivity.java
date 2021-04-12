ae244573ec760298418478692d252cd4cd12362f
Andrew Bare <andrewebare21@yahoo.com>
2021-04-12 14:42:16 -0400

    create popup window when get points is clicked

--- app/src/main/java/com/example/foodfinderapp/BusinessActivity.java
+++ app/src/main/java/com/example/foodfinderapp/BusinessActivity.java
@@ -2,7 +2,14 @@ package com.example.foodfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +24,7 @@ public class BusinessActivity extends AppCompatActivity {
     TextView m_businessName;
     TextView m_businessDescription;
     Button m_setIfSavedBtn;
+    Button m_getPointsButton;
 
     // Called when activity is created
     @Override
@@ -27,6 +35,10 @@ public class BusinessActivity extends AppCompatActivity {
         m_businessName = (TextView) findViewById(R.id.businessNameTV);
         m_businessDescription = (TextView) findViewById(R.id.businessDescriptionTV);
         m_setIfSavedBtn = (Button) findViewById(R.id.setIfSavedBtn);
+
+        //popup code entry
+        m_getPointsButton = (Button) findViewById(R.id.getPointsBtn);
+        m_getPointsButton.setOnClickListener(this::onEnterPointsButtonClick);
 
         // Check if business object was successfully passed
         if (getIntent().getExtras() != null) {
@@ -63,6 +75,42 @@ public class BusinessActivity extends AppCompatActivity {
         } else {
             m_setIfSavedBtn.setText("x");
         }
+    }
+
+    public void onEnterPointsButtonClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.get_points_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, 400, true);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        Button enterCode = (Button) popupView.findViewById(R.id.getPointsGO);
+        enterCode.setOnClickListener(this::onCodeEnterClick);
+
+    }
+
+    public void onCodeEnterClick(View view){
+        System.out.println("IM HERE");
+        //TODO shoot a request to database
+        //TODO wait for response
+
+        boolean goodResponse = false;
+        if (goodResponse){
+            //TODO add points
+
+        }else{
+            //Tell user they entered a wrong number
+            Toast.makeText(this, "Code Entered not Found!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
